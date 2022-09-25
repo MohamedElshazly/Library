5b2242fd80bf0b79f5ef2f1a6aafbb68a37892f7
omni <mohamedelshazly18@yahoo.com>
2022-09-26 00:49:07 +0200

    added functioning id system, implemented a delete book btn that deletes from the array database and dom

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let myLibrary = []; 
+let myLibrary = [];
 let globalID = 0;
 
-function Book(title, author, numPages, read=false, ID=globalID) {
-    this.title = title; 
-    this.author = author; 
+function Book(title, author, numPages, read = false, ID = globalID) {
+    this.title = title;
+    this.author = author;
     this.numPages = numPages;
-    this.read = read; 
+    this.read = read;
     this.getID = () => {
         return ID;
     };
 
     this.toggleRead = () => {
-        this.read = !(this.read); 
+        this.read = !(this.read);
     }
 }
 
@@ -21,14 +21,14 @@ const addNewBook = () => {
         author: '',
         numPages: 0,
         read: false
-    }; 
+    };
     bookArguments.title = document.getElementById('title').value;
     bookArguments.author = document.getElementById('author').value;
     bookArguments.numPages = document.getElementById('num').value;
     bookArguments.read = document.getElementById('read').checked;
     const book = new Book(bookArguments.title, bookArguments.author, bookArguments.numPages, bookArguments.read);
     myLibrary.push(book);
-    globalID += 1;  
+    globalID += 1;
 };
 
 
@@ -43,42 +43,44 @@ const closeForm = () => {
     document.querySelector('.addBookForm').classList.remove('dim');
     document.querySelector('.addBookForm').reset();
 };
-const createAndAppendCard = (title, author, numPages, read) => {
-    
+const createAndAppendCard = (title, author, numPages, read, id) => {
+
     //parent
     const card = document.createElement("div");
     card.classList.add('card');
-    
+    card.id = id;
+
     //card content
     const cardContent = document.createElement("div");
     cardContent.classList.add('card-content');
 
-    
+
 
     //populate card content
     const h3 = document.createElement('h3');
     h3.textContent = title;
     cardContent.appendChild(h3);
-    
+
     const h4 = document.createElement('h4');
-    h4.textContent = `by ${author}`; 
-    cardContent.appendChild(h4); 
+    h4.textContent = `by ${author}`;
+    cardContent.appendChild(h4);
 
     const p1 = document.createElement('p');
     p1.textContent = `${numPages} pages`;
     cardContent.appendChild(p1);
 
     const p2 = document.createElement('p');
-    
-    const checkbox = document.createElement('input'); 
+
+    const checkbox = document.createElement('input');
     checkbox.setAttribute("type", "checkbox");
     checkbox.setAttribute("name", "card-read");
     checkbox.setAttribute("id", "card-read");
+    checkbox.onclick = () => {myLibrary[card.id].toggleRead()}
     checkbox.checked = read;
-    
+
     const checkboxLabel = document.createElement('label');
-    checkboxLabel.textContent = 'Read?'; 
-    checkboxLabel.setAttribute("for", "card-read"); 
+    checkboxLabel.textContent = 'Read?';
+    checkboxLabel.setAttribute("for", "card-read");
 
     p2.appendChild(checkbox);
     p2.appendChild(checkboxLabel);
@@ -92,7 +94,7 @@ const createAndAppendCard = (title, author, numPages, read) => {
     const delButton = document.createElement('button');
     delButton.classList.add('alert');
     delButton.textContent = 'Delete Book';
-    
+
     card.appendChild(delButton);
 
     return card;
@@ -101,33 +103,44 @@ const createAndAppendCard = (title, author, numPages, read) => {
 const appendCardToDOM = () => {
     //get root cards div 
     let cards = document.querySelector('.cards');
-    for (let i = globalID - 1; i < myLibrary.length; i++){
-        const card = createAndAppendCard(myLibrary[i].title, myLibrary[i].author, myLibrary[i].numPages, myLibrary[i].read);
+    for (let i = myLibrary.length - 1; i < myLibrary.length; i++) {
+        const cardID = myLibrary[i].getID();
+        const card = createAndAppendCard(myLibrary[i].title, myLibrary[i].author, myLibrary[i].numPages, myLibrary[i].read, cardID);
         cards.appendChild(card);
     }
+}
+
+const deleteBook = (id) => {
+    myLibrary.splice(id, 1);
+    let parentElement = document.getElementById(id);
+    parentElement.parentNode.removeChild(parentElement); 
 }
 
 
 
 
 //events 
-const addBtn = document.querySelector('.addBook'); 
+const addBtn = document.querySelector('.addBook');
 addBtn.addEventListener('click', () => {
-   addNewBook();
-   appendCardToDOM();
-}); 
-
-const pageTitle = document.querySelector('h1'); 
-
-pageTitle.addEventListener('click', () => {
-    console.log(myLibrary);
+    addNewBook();
+    appendCardToDOM();
 });
 
-const form = document.querySelector('.addBookForm'); 
+const form = document.querySelector('.addBookForm');
 const handleSubmit = (e) => {
     e.preventDefault();
-    // addNewBook(); 
     closeForm();
 };
 form.addEventListener('submit', handleSubmit);
 
+//event for deleting cards
+const cardsContainer = document.querySelector('.cards');
+// const deleteBookBtn = document.querySelector('.alert');
+cardsContainer.addEventListener('click', (e) => {
+    if(e.target && e.target.matches(".alert")){
+        const deleteBookBtn = e.target;
+        const id = deleteBookBtn.parentElement.id;
+        deleteBook(id); 
+    }
+})
+    
